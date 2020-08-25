af9fab13d2d2563e9a9bdd279fcd8441822a26b7
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-25 13:49:44 -0700

    Reformat HTML side, add business logical functions
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com>
    
    Co-authored-by: Brian Harvey <brian.harv3y@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,66 @@
-function BoxOffice () {
-  this.tickets = [];
-}
-BoxOffice.prototype.addTicket = function (ticket) {
-  this.tickets.push(ticket)
-}
+// function BoxOffice () {
+//   this.tickets = [];
+// }
+// BoxOffice.prototype.addTicket = function (ticket) {
+//   this.tickets.push(ticket)
+// }
+
+
 //Business Logic for Ticket------
-function Ticket(Time, Age, nameOfMovie) {
+function Ticket(Time, Age, movieAge) {
   this.time = Time,
   this.age = Age,
-  this.nameOfMovie = nameOfMovie;
+  this.movieAge = movieAge;
 }
 
-function price(){
-  if Age <= 12 
+Ticket.prototype.discount = function() {
+  if (this.age >= 65 || this.age < 12) {
+    return true;
+  } else {
+    return false;
+  };
 }
+  
+Ticket.prototype.isNew = function () {
+    if (this.movieAge <= 2) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+Ticket.prototype.Time = function () {
+  if (this.time < 12) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+Ticket.prototype.price = function () {
+  let start = 20;
+
+  if (this.discount() === true) {
+    start = start - 5;
+  }
+
+  if (this.isNew() === true) {
+    start = start + 5;
+  } 
+
+  if (this.Time() === true) {
+    start = start - 5;
+  }
+  return start;
+}
+
 
 Ticket.prototype.ticketOutput = function() {
-  return this.time + this.age + this.nameOfMovie;
+  return this.time + this.age + this.nameOfMovie + this.Price;
 }
+
+//get inputs
+//pass inputs into ticket
+//pass inputs through the price function
+
